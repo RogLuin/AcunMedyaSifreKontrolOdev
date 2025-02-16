412ea22856b235adf991377147febe9d96be76dc
Gorkem Dikbakan <gorkem_gd@hotmail.com>
2025-02-16 21:06:32 +0300

    Acun Medya Sifre Kontrol Odevi

---
+++ SifreGüclülügüKontrol/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Şifrenizi girin: ");
+string sifre = Console.ReadLine();
+
+if (sifre.Length < 8)
+{
+    Console.WriteLine("Hata: Şifre en az 8 karakter olmalıdır!");
+    return;
+}
+
+if (!(sifre.Contains("%") || sifre.Contains("&") || sifre.Contains("*")))
+{
+    Console.WriteLine("Hata: Şifre en az bir özel karakter (% , & , *) içermelidir!");
+    return;
+}
+
+if (!sifre.Any(char.IsUpper))
+{
+    Console.WriteLine("Hata: Şifre en az bir büyük harf içermelidir!");
+    return;
+}
+
+Console.WriteLine("Şifre Güçlü. Şifre başarıyla oluşturuldu.");
